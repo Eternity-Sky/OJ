121e5bb21b4726bc71a4fd20870e7b578874cc2e
Eternity-Sky <tengyumo666@hotmail.com>
2024-12-12 20:56:07 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// 设置静态文件目录
+app.use(express.static('public')); // 这行代码确保可以访问 public 目录下的文件
+
 // 设置视图引擎为 EJS
 app.set('view engine', 'ejs');
 app.set('views', './views'); // 设置视图文件夹
