3b5aa559f2d163eceb0796fb988b0fc211297e93
Eternity-Sky <tengyumo666@hotmail.com>
2024-12-12 13:00:30 +0800

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,14 +1,24 @@
-const http = require('http');
+const express = require('express');
+const path = require('path');
+const app = express();
 
-// 创建一个简单的 HTTP 服务器
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('欢迎来到 OJ 动态网站！\n');
+// 设置 EJS 作为视图引擎
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// 静态文件托管
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 路由
+app.get('/', (req, res) => {
+    res.render('index'); // 渲染 index.ejs
 });
 
-// 服务器监听端口 8080
-const PORT = 8080;
-server.listen(PORT, () => {
+// 其他路由和中间件
+// ...
+
+// 服务器监听端口
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
     console.log(`服务器正在端口 ${PORT} 上运行`);
 }); 
